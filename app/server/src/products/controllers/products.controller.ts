e3de8563fa080b821ae692ec424d34576eb49eb3
julio-silveira <julioqueiroz15@gmail.com>
2023-05-13 16:14:22 -0300

    feat: csv parser

--- app/server/src/products/controllers/products.controller.ts
+++ app/server/src/products/controllers/products.controller.ts
@@ -1,3 +1,4 @@
+import { csvParser } from '@/utils/csv.parser';
 import {
   Controller,
   Post,
@@ -32,7 +33,7 @@ export class ProductsController {
   }
 
   @Get()
-  findAll() {
+  async findAll() {
     return this.productsService.findAll();
   }
 
@@ -48,8 +49,17 @@ export class ProductsController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
+  async update(
+    @Param('id') id: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    //temporary
+    console.log(updateProductDto);
+
+    const teste = await csvParser('../preco.csv');
+    console.log(teste);
+
+    return teste;
   }
 
   @Delete(':id')
--- app/server/src/utils/csv.parser.ts
+++ app/server/src/utils/csv.parser.ts
@@ -1,3 +1,19 @@
-export const csvParser = (file) => {
-  console.log('csvParser');
+import { parse } from 'csv-parse';
+import * as fs from 'fs';
+import * as path from 'path';
+
+export const csvParser = async (file: string) => {
+  const PATH = path.join(__dirname, '../../', '/src/utils/preco.csv');
+
+  return new Promise((resolve) => {
+    const buffer = [];
+    fs.createReadStream(PATH)
+      .pipe(parse({ delimiter: ',', from_line: 2 }))
+      .on('data', (row) => {
+        buffer.push({ productId: row[0], newPrice: row[1] });
+      })
+      .on('end', () => {
+        resolve(buffer);
+      });
+  });
 };
