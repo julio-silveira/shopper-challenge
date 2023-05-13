a019e04d82aa41cde36aabe825226146e018415b
julio-silveira <julioqueiroz15@gmail.com>
2023-05-13 13:44:01 -0300

    test: product findOne Service

--- app/server/src/app.module.ts
+++ app/server/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { PrismaService } from './prisma/prisma.service';
 import { ConfigModule } from '@nestjs/config';
-import configuration from './config/configuration';
 
 @Module({
   imports: [ConfigModule.forRoot(), ProductsModule],
--- app/server/src/products/services/products.service.spec.ts
+++ app/server/src/products/services/products.service.spec.ts
@@ -1,11 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsService } from './products.service';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { Product } from '@prisma/client';
+import { PrismaService } from '../../prisma/prisma.service';
+import { Prisma, Product } from '@prisma/client';
 
 describe('ProductsService', () => {
   let service: ProductsService;
   let prisma: PrismaService;
+
+  const product: Product = {
+    code: 100,
+    name: 'SODA',
+    costPrice: new Prisma.Decimal(10),
+    salesPrice: new Prisma.Decimal(20),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -24,35 +31,22 @@ describe('ProductsService', () => {
     }).compile();
 
     service = module.get<ProductsService>(ProductsService);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  describe('findOne', async () => {
+  describe('findOne', () => {
     it('should return a product', async () => {
-      const product: Product = {
-        code: 100,
-        name: 'SODA',
-        costPrice: 10,
-        salesPrice: 20,
-      };
-
-      jest.spyOn(prisma.product, 'findUnique').mockReturnValue(product);
+      jest.spyOn(prisma.product, 'findUnique').mockResolvedValue(product);
 
       const response = await service.findOne(product.code);
       expect(response).toEqual(product);
     });
 
     it('should return a null when a product with code not registered', async () => {
-      const product = {
-        code: 100,
-        name: 'SODA',
-        costPrice: 10,
-        salesPrice: 20,
-      };
-
       jest.spyOn(prisma.product, 'findUnique').mockReturnValue(null);
 
       const response = await service.findOne(product.code);
@@ -60,15 +54,8 @@ describe('ProductsService', () => {
     });
   });
 
-  describe('create', async () => {
+  describe('create', () => {
     it('should create a product', async () => {
-      const product = {
-        code: 100,
-        name: 'SODA',
-        costPrice: 10,
-        salesPrice: 20,
-      };
-
       const message = await service.create(product);
       expect(message).toEqual('Producto cadastrado com sucesso');
     });
