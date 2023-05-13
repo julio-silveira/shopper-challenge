df6893c8d821ede7021603cad0f8e5e6c46743a3
julio-silveira <julioqueiroz15@gmail.com>
2023-05-13 12:25:04 -0300

    test: TDD- product create service

--- app/server/src/products/services/products.service.spec.ts
+++ app/server/src/products/services/products.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsService } from './products.service';
+import { Prisma } from '@prisma/client';
 
 describe('ProductsService', () => {
   let service: ProductsService;
@@ -14,5 +15,33 @@ describe('ProductsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', async () => {
+    it('should create a product', async () => {
+      const product = {
+        code: 100,
+        name: 'SODA',
+        costPrice: 10,
+        salesPrice: 20,
+      };
+
+      const message = await service.create(product);
+      expect(message).toEqual('Producto cadastrado com sucesso');
+    });
+
+    it('should not create a product with code already registered', async () => {
+      const product = {
+        code: 100,
+        name: 'SODA',
+        costPrice: 10,
+        salesPrice: 20,
+      };
+
+      await service.create(product);
+      await expect(service.create(product)).rejects.toThrow(
+        'Código já cadastrado',
+      );
+    });
   });
 });
