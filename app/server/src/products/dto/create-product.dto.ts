5a8c2e51ff47efb414f9afaf2c3db406fe75fe9b
julio-silveira <julioqueiroz15@gmail.com>
2023-05-13 13:49:40 -0300

    feat: create product dto

--- app/server/src/products/dto/create-product.dto.ts
+++ app/server/src/products/dto/create-product.dto.ts
@@ -1,1 +1,19 @@
-export class CreateProductDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateProductDto {
+  @IsNotEmpty({ message: 'O código é obrigatório' })
+  @IsNumber({}, { message: 'O código deve ser um número' })
+  code: number;
+
+  @IsNotEmpty({ message: 'O nome é obrigatório' })
+  @IsString({ message: 'O nome deve ser uma string' })
+  name: string;
+
+  @IsNotEmpty({ message: 'O preço de custo é obrigatório' })
+  @IsNumber({}, { message: 'O preço de custo deve ser um número' })
+  costPrice: number;
+
+  @IsNotEmpty({ message: 'O preço de venda é obrigatório' })
+  @IsNumber({}, { message: 'O preço de venda deve ser um número' })
+  salesPrice: number;
+}
